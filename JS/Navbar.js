c9153bbbf8a015feff8de315cc6d9069aefb082d
Ophidia <danlines02@gmail.com>
2024-10-18 01:04:47 +0100

    Updated Titles and removed Splash

--- JS/Navbar.js
+++ JS/Navbar.js
@@ -1,24 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Fetch and insert the navbar
   fetch("NAVBAR/NAV.html")
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById("Nav_Bar").innerHTML = data;
-      });
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById("Nav_Bar").innerHTML = data;
+  });
 
-  // Handle the splash screen
-  let splash_ran = localStorage.getItem('splashRan') === 'true';
-  const splash = document.querySelector('.splash');
 
-  if (!splash_ran) {
-      // If splash hasn't run before, show it and set up the animation end listener
-      splash.style.display = 'block'; // Ensure it's visible
-      splash.addEventListener('animationend', () => {
-          splash.style.display = 'none';
-          localStorage.setItem('splashRan', 'true');
-      });
-  } else {
-      // If splash has run before, remove it immediately
-      splash.remove();
-  }
+
 });
